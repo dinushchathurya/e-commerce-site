f4aeb754f025e9320c753d9908f4357e7d156c45
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-24 22:49:37 +0530

    Create Default.js

---
+++ src/components/Default.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Default extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Page not Found</h3>
+            </div>
+        )
+    }
+}
