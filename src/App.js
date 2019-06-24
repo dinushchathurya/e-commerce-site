f841d97de8bc037e44e4489a5b14883b46d2961a
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-24 22:06:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="container">
-       <div className="row">
-         <div className="col-6">Column number</div>
-         <div className="col-6">Column number</div>
-       </div>
-    </div>
+    <React.Fragment>
+      <h3>hello from app</h3>
+    </React.Fragment>
   );
 }
 
