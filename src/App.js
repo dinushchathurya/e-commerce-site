bb8ea833b88771983f2c5b934c00f561e277d775
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-24 22:49:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import {Switch,Route} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 function App() {
   return (
     <React.Fragment>
-      <h3>hello from app</h3>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" components={Cart} />
+        <Route components={Default} />
+      </Switch>
+      <ProductList />
+      <Details />
+      <Cart />
+      <Default />
     </React.Fragment>
   );
 }
