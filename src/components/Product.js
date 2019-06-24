8cf23d54a104615fb67a4f58560aed49c37c18a0
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-24 22:49:53 +0530

    Create Product.js

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Product extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Product</h3>
+            </div>
+        )
+    }
+}
