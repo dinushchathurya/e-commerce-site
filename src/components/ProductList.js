a7fac14eb65ffb5401621710e17b6916eed6b4ba
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-24 22:06:18 +0530

    Create ProductList.js

---
+++ src/components/ProductList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProductList extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Prduct List</h3>
+            </div>
+        )
+    }
+}
