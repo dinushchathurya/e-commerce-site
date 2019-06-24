3da0d1a6dea4ff3ecdf8616ef21862ca57516b53
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-24 22:06:26 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Navbar</h3>
+            </div>
+        )
+    }
+}
+
