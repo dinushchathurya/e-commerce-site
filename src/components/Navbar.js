42419220e62fcaefd9ef52c8fa103665523878fc
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-25 09:54:06 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
+import styled from 'styled-components'
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from Navbar</h3>
-            </div>
+            <nav className="navbar nav-bar-expand-sm bg-primary navbar-dark px-sm-5">
+               <Link to='/'>
+                  <img src={logo} alt="store" className="navbar-brand" />
+               </Link> 
+               <ul className="navbar-nav align-items-center">
+                 <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                       Products
+                    </Link>
+                 </li>
+               </ul>
+               <Link to="/cart" className="ml-auto">
+                  <ButtonContainer>
+                      <i className="fas fa-cart-plus">my cart</i>
+                  </ButtonContainer>
+               </Link>
+            </nav>
         )
     }
 }
-
+const ButtonContainer = styled.button`
+text-transform:capitalize;
+`;
