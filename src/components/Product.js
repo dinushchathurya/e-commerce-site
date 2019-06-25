2453bb3762088e2453340aa6186005bdfb95e908
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-25 23:46:55 +0530

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,18 +10,21 @@ export default class Product extends Component {
         return (
             <ProducrWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                <div className="card">
-                 <div className="img-container p-5" onClick={()=>console.log('click me')}>
-                    <Link to="/details">
-                     <img src={img} alt="product"className="card-img-top" />
-                    </Link>
-                    <button className="cart-btn" disabled={inCart ? true :false}
-                    onClick={()=>{console.log("added to the cart");
-                    }}>
-                            {inCart ? (<p className="text-capitalize mb-0" disabled>{""}in Cart</p>)
-                                : (<i className="fas fa-cart-plus" />)}
-                    </button>
-                    
-                 </div>
+                <ProductConsumer>
+                        {value => (<div className="img-container p-5" onClick={() => value.handleDetail(id)}>
+                            <Link to="/details">
+                                <img src={img} alt="product" className="card-img-top" />
+                            </Link>
+                            <button className="cart-btn" disabled={inCart ? true : false}
+                                onClick={() => {
+                                    value.addToCart(id);
+                                    value.openModal(id);
+                                }}>
+                                {inCart ? (<p className="text-capitalize mb-0" disabled>{""}in Cart</p>)
+                                    : (<i className="fas fa-cart-plus" />)}
+                            </button>
+                        </div>)}
+                </ProductConsumer>
                  <div className="card-footer d-flex justify-content-between">
                      <p className="align-self-center mb-0">
                          {title}
