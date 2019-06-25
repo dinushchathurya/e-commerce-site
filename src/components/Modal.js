9192d6f6fe5efe4acd2139b888032c7b5e17e54a
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-26 00:03:16 +0530

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import styled from 'styled-components';
+import {ProductConsumer} from '../context';
+import {ButtonContainer} from './Button';
+import {Link} from 'react-router-dom';
 
 export default class Modal extends Component {
     render() {
         return (
-            <div>
-                <h3>hello from modal</h3>
-            </div>
+            <ProductConsumer>
+                {(value)=>{
+                    const {modalOpen,closeModal} =value;
+                    const {img, title,price} = value.modalProduct;
+                    if(!modalOpen){
+                        return null;
+                    }else{
+                        <ModalContainer>
+                          <div className="container">
+                             <div className="row">
+                             
+                             </div> 
+                          </div>
+                        </ModalContainer>
+                    }
+                }}
+            </ProductConsumer>
         )
     }
 }
+const ModalContainer =styled.div``
+
+;
