8ed24951cf0b5804580049ab86842dccedd525f7
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-25 15:10:55 +0530

    Create context.js

---
+++ src/context.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import {storeProducts, detailProduct} from './data';
+
+const ProductContext = React.createContext();
+
+class ProductProvider extends Component {
+    state ={
+        products:[],
+        detailProduct:detailProduct
+    };
+    componentDidMount(){
+        this.setProducts();
+    }
+    setProducts=()=>{
+        let tempProducts = [];
+        storeProducts.forEach(item =>{
+         const singleItem = {...item};
+         tempProducts = [...tempProducts,singleItem];
+
+        })
+        this.setState(()=>{
+            return {products:tempProducts}
+        })  
+    }
+    handleDetail = ()=>{
+        console.log('hello from detail');
+    }
+    addToCart = () => {
+        console.log('hello from add to cart');
+    }
+    render() {
+        return (
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetail:this.handleDetail,
+                addToCart:this.addToCart
+            }}>
+              {this.props.children}  
+            </ProductContext.Provider>
+        )
+    }
+}
+const ProductConsumer = ProductContext.Consumer;
+export {ProductProvider,ProductConsumer};
