a01a8356b98c7d274045e81fbb45734da5fd3f7c
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-25 15:11:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
+import {ProductProvider} from './context';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <ProductProvider>
+        <Router>
+            <App />
+        </Router>
+    </ProductProvider>,
      document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
