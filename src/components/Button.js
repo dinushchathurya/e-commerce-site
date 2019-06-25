f5356a312a34b948b323903c1366fc405f167590
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-25 22:30:41 +0530

    create add to cart button

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,14 +5,15 @@ text-transform:capitalize;
 font-size:1.4rem;
 background:transparent;
 border:0.05rem solid var(--lightBlue);
+border-color:${props => props.cart ? "var(--mainYellow)" :"var(--lightBlue)"};
 border-radius:0.5rem;
-color:var(--lightBlue);
+color:${props => props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
 padding:0.2rem 0.5rem;
 cursor:pointer;
 margin:0.2rem 0.5rem 0.2rem 0;
 transition:all 0.5s ease-in-out;
 &:hover{
-    background:var(--lightBlue);
+    background:${props => props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
     color:var(--mainBlue);
 }
 :focus{
