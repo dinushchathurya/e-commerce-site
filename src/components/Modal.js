ef33b06364e06556e35eb85185f0b1f1de6f0968
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-25 23:45:55 +0530

    Create Modal.js

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Modal extends Component {
+    render() {
+        return (
+            <div>
+                <h3>hello from modal</h3>
+            </div>
+        )
+    }
+}
