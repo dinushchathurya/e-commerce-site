10653c93c34935db04d36c7e4e0ba0f9d532186f
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-25 22:30:21 +0530

    fixed value passing error

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,20 +7,57 @@ export default class Details extends Component {
     render() {
         return (
             <ProductConsumer>
-                {(value)=>{
+                {value=>{
                    const {id, company, img, info, price, title, inCart} = value.detailProduct; 
                    return(
                        <div className="container py-5">
+                           {/*title*/}
                             <div className="row>">
                                 <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
                                     <h1>{title}</h1>
                                 </div>
                             </div>
+                            {/*end of title*/}
+                            {/*product info*/}
                             <div className="row">
-                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize"/>
+                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                    <img src={img} className="img-fluid" alt="product" />
+                                </div>
+                            {/*product text*/}
+                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                    <h2>model:{title}</h2>
+                                    <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                                        made by: <span className="text-uppercase">{company}</span>
+                                    </h4>
+                                    <h4 className="text-blue">
+                                        <strong>
+                                            Price : <span>$</span>{price}
+                                        </strong>
+                                    </h4>
+                                    <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                                            some info about product
+                                    </p>
+                                    <p className="text-muted lead">
+                                        {info}
+                                    </p>
+                                    {/*buttons*/}
+                                    <div>
+                                       <Link to="/">
+                                           <ButtonContainer>
+                                               back to products
+                                           </ButtonContainer>
+                                        </Link>
+                                            <ButtonContainer cart disabled={inCart ? true : false}
+                                                onClick={() => {value.addToCart(id);}}>
+                                               {inCart ? "inCart" : "add to cart"}
+                                            </ButtonContainer>
+                                    </div>
+                                </div>
                             </div>
-                       </div>
-                   )
+                        </div>
+                      
+                       
+                   ); 
                 }}
             </ProductConsumer>
         )
