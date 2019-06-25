d9bacd8bae8f95326fc2d5833582d5babf92804b
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-25 15:10:33 +0530

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import Product from "./Product";
+import Title from "./Title";
+import {ProductConsumer} from '../context';
 
 export default class ProductList extends Component {
     render() {
         return (
-            <div>
-                <Product>
-
-                </Product>
-            </div>
-        )
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                    <Title name="our" title="products"/>
+                       <div className="row">
+                         <ProductConsumer>
+                             {(value)=>{
+                               return value.products.map(product =>{
+                                   return <Product key={product.id} product={product} />;
+                               });
+                             }}
+                         </ProductConsumer>
+                       </div>
+                       </div> 
+                    </div>
+            </React.Fragment>
+        );
     }
 }
