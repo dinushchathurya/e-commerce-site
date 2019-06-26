1f60daf9fd4bdb61a3acbe8d00a6c72bbf265124
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-26 18:05:01 +0530

    Update EmptyCart.js

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -4,7 +4,7 @@ export default function EmptyCart() {
     return (
         <div className="container mt-5">
             <div className="row">
-               <div className="col-10 mx-auto text-center text-title">
+                <div className="col-10 mx-auto text-center text-title">
                   <h1>Your cart is currently empty</h1> 
                </div>
             </div>
