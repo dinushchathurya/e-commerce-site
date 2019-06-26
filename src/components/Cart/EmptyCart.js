c1e3b2fc0fa5e022e0bea46832182fafb2baebe5
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-26 14:51:08 +0530

    Create EmptyCart.js

---
+++ src/components/Cart/EmptyCart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function EmptyCart() {
+    return (
+        <div className="container mt-5">
+            <div className="row">
+               <div className="col-10 mx-auto text-center text-title">
+                  <h1>Your cart is currently empty</h1> 
+               </div>
+            </div>
+        </div>
+    );
+}
