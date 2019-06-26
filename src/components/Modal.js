25bf488672dbadc794b57fd19396cac9a794907b
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-26 11:09:00 +0530

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -14,19 +14,49 @@ export default class Modal extends Component {
                     if(!modalOpen){
                         return null;
                     }else{
-                        <ModalContainer>
-                          <div className="container">
-                             <div className="row">
-                             
-                             </div> 
-                          </div>
+                        return (
+                            < ModalContainer >
+                            <div className="container">
+                                <div className="row">
+                                    <div id="modal" className=
+                                    "col-8 mx-auto col-md-6 col-lg-4 text-capitalize text-center p-5">
+                                    <h5>item added to the cart</h5>
+                                    <img src={img} className="img-fluid" alt="product" />
+                                    <h5>{title}</h5>
+                                    <h5 className="text-muted">price : $ {price}</h5>
+                                    <Link to='/'>
+                                    <ButtonContainer onClick={()=>closeModal()}>
+                                        continue shopiing
+                                    </ButtonContainer>
+                                    </Link>
+                                    <Link to='/cart'>
+                                    <ButtonContainer cart onClick={() => closeModal()}>
+                                         go to cart
+                                    </ButtonContainer>
+                                    </Link>
+                                    </div>
+                                </div>
+                            </div>
                         </ModalContainer>
-                    }
-                }}
+                        );
+            }
+                
+        }}
             </ProductConsumer>
-        )
+        );
     }
 }
-const ModalContainer =styled.div``
-
-;
+const ModalContainer =styled.div`
+position:fixed;
+top:0;
+left:0;
+right:0;
+bottom:0;
+background:rgba(0,0,0,0.3);
+display:flex;
+align-items:center;
+justify-content:center;
+#modal{
+    background:var(--mainWhite);
+}
+`;
