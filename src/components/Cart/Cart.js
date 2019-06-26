9ee1b23b62816d4181803c65213b8d8b522f830a
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-26 14:50:44 +0530

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import Title from '../Title';
+import CartColumns from "./CartColumns";
+import EmptyCart from "./EmptyCart";
+import {ProductConsumer} from "../../context";
 
 export default class Cart extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from Cart</h3>
-            </div>
-        )
+           <section>
+               <ProductConsumer>
+                   {value =>{
+                       const { cart } = value;
+                       if(cart.lenght> 0){
+                           return (
+                               <React.Fragment>
+                                   <Title name="your" title="cart" />
+                                   <CartColumns/>
+                               </React.Fragment>
+                              );
+                       }
+                       else{
+                           return <EmptyCart />;
+                       }
+                   }}
+               </ProductConsumer>
+           </section>
+        );
     }
 }
