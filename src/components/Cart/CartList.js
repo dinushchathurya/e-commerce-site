131516e5d3c2c21a556e0875f2c7d5977d4aba12
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-26 14:51:02 +0530

    Create CartList.js

---
+++ src/components/Cart/CartList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CardItem from './CartItem';
+
+export default class CartList extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hell from Cart List</h3>
+            </div>
+        )
+    }
+}
