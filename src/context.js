ce876b6f7b27a6a5be58ad200734cec02b79705c
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-26 11:08:41 +0530

    Create cart functions

--- src/context.js
+++ src/context.js
@@ -9,8 +9,11 @@ class ProductProvider extends Component {
         products:[],
         detailProduct:detailProduct,
         cart:[],
-        modalOpen:true,
+        modalOpen:false,
         modalProduct:detailProduct,
+        cartSubTotal:0,
+        cartTax:0,
+        cartTotal:0
     };
     componentDidMount(){
         this.setProducts();
@@ -60,6 +63,18 @@ class ProductProvider extends Component {
             return {modalOpen:false}
         })
     }
+    increment = (id)=>{
+        console.log("this increment");
+    }
+    decrement = (id) => {
+        console.log("this decrement");
+    }
+    removeItem = (id)=>{
+        console.log("this is remome");
+    }
+    clearCart = ()=>{
+        console.log("cart was cleared");
+    }
     render() {
         return (
             <ProductContext.Provider value={{
@@ -67,7 +82,11 @@ class ProductProvider extends Component {
                 handleDetail:this.handleDetail,
                 addToCart:this.addToCart,
                 openModal:this.openModal,
-                closeModal:this.closeModal
+                closeModal:this.closeModal,
+                increment:this.increment,
+                decrement:this.decrement,
+                removeItem:this.removeItem,
+                clearCart:this.clearCart
             }}>
               {this.props.children}  
             </ProductContext.Provider>
