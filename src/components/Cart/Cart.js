6b19f35daa2ef6184d44ca3c8ee191de9246c209
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-26 11:09:13 +0530

    Create Cart.js

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Cart extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Cart</h3>
+            </div>
+        )
+    }
+}
