074c0e41786e4008756a276034fc01c0f444d725
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-27 19:07:58 +0530

    Deploy location path name

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react'
 
 export default class Default extends Component {
     render() {
+        console.log(this.props);
         return (
-            <div>
-                <h3>Page not Found</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto text-center text-title">
+                        <h1>error</h1>
+                        <h1 className="display-3">404</h1>
+                        <h2>page not found</h2>
+                        <h3>the requested URL 
+                            <span className="text-danger">
+                              {this.props.location.pathname}
+                            </span>{""}
+                            was not found</h3>
+                    </div>
+                </div>
             </div>
         )
     }
