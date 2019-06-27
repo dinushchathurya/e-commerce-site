2c964b56bc439c040fccf98e06cbdb7988cd65dd
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-27 19:08:09 +0530

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,33 +1,35 @@
 import React, { Component } from 'react'
 import Title from '../Title';
-import CartColumns from "./CartColumns";
-import EmptyCart from "./EmptyCart";
-import {ProductConsumer} from "../../context";
+import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
+import { ProductConsumer} from '../../context';
 import CartList from './CartList';
-import CartTotals from './CartTotals';
+
 
 export default class Cart extends Component {
     render() {
         return (
-           <section>
-               <ProductConsumer>
-                   {value =>{
-                       const { cart } = value;
-                       if(cart.lenght > 0){
-                           return (
-                               <React.Fragment>
-                                   <Title name="your" title="cart" />
-                                   <CartColumns/>
-                                   <CartList value={value} />
-                               </React.Fragment>
-                              )
-                       }
-                       else {
-                           return <EmptyCart />;
-                       }
-                   }}
-               </ProductConsumer>
-           </section>
-        );
+            <section>
+                <ProductConsumer>
+                    {value => {
+                        const {cart} =value;
+                        if (cart.lenght>0){
+                            return (
+                                <React.Fragment>
+                                    < Title name="your" title="cart" />
+                                    <CartColumns />
+                                    <CartList value={value} />
+                                </React.Fragment>
+                                
+                            );
+                        }
+                        else {
+                            return <EmptyCart />
+                        }
+                    }}
+                </ProductConsumer>
+                
+            </section>
+        )
     }
 }
