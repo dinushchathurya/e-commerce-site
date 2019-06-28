34fba3febc136218bd0f2de584eeeb813ee23e2f
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-28 12:41:08 +0530

    Fixed cart not open error

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,32 +4,30 @@ import CartColumns from './CartColumns';
 import EmptyCart from './EmptyCart';
 import { ProductConsumer} from '../../context';
 import CartList from './CartList';
+import CartTotals from "./CartTotals";
 
-
-export default class Cart extends Component {
+export default class Store extends Component {
     render() {
         return (
             <section>
                 <ProductConsumer>
                     {value => {
-                        const {cart} =value;
-                        if (cart.lenght>0){
+                        const { cart } = value;
+                        if (cart.length > 0) {
                             return (
                                 <React.Fragment>
-                                    < Title name="your" title="cart" />
+                                    <Title name="your" title="cart" />
                                     <CartColumns />
                                     <CartList value={value} />
+                                    <CartTotals value={value} history={this.props.history} />
                                 </React.Fragment>
-                                
                             );
-                        }
-                        else {
-                            return <EmptyCart />
+                        } else {
+                            return <EmptyCart />;
                         }
                     }}
                 </ProductConsumer>
-                
             </section>
-        )
+        );
     }
 }
