d2e17b171dcd988ae352bd76d1b896ae90fe7a55
dinushchathurya <dinushchathurya21@gmail.com>
2019-06-28 12:45:58 +0530

    Fixed clear cart  button error

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -8,9 +8,14 @@ export default function CartTotals({value}) {
            <div className="row">
                <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
                     <Link to="/">
-                      <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={()=>clearCart}>
-                          clear cart 
-                      </button>
+                        <button
+                            className="btn btn-outline-danger text-uppercase mb-3 px-5"
+                            type="button"
+                            onClick={() => {
+                                clearCart();
+                            }}>
+                            clear cart
+                        </button>
                     </Link>
                     <h5>
                         <span className="text-title">subtotal :</span>
