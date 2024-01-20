07c7577ef765ba1787ae6ac42233a2602bf4f197
jibinshan <jibinshanu23@gmail.com>
2024-01-20 11:47:19 +0530

    updated with Nightmode.moved Nightmode icon on right corner of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -79,20 +79,21 @@ class Navbar extends Component {
               <img src={logo} alt="store" className="navbar-brand" />
             </Link>
             <ul className="navbar-nav align-items-center">
-              <li className="nav-item ml-5 mainmenu">
+              <li className="nav-item ml-5">
                 <Link to="/" className="nav-link">
                   Products
                 </Link>
-                <div className="text-white bg-transparent themes" onClick={toggleTheme}>
-                  {theme ? <FaRegMoon /> : <GoSun />}
-                </div>
               </li>
             </ul>
             <Link to="/cart" className="ml-auto">
+
               <ButtonContainer>
                 <i className="fas fa-cart-plus">my cart</i>
               </ButtonContainer>
             </Link>
+              <div className="text-white bg-transparent themes mainmenu" onClick={toggleTheme}>
+                  {theme ? <FaRegMoon /> : <GoSun />}
+              </div>
           </DesktopNavWrapper>
         )}
       </div>
